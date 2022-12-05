3fe1d86ab702e6eede77281a2280d5b4499e3faf
Matheus <matheuskael.silva@gmail.com>
2022-12-05 19:22:34 +0000

    Mon, Dec 5, 2022, 7:22 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,33 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void make_bricks(int quantity_of_bricks, int height, bool right_side);
+void make_bricks(int quantity_of_bricks);
 
 int main(void)
 {
     int height = get_int("Height: ");
     for (int i = 0; i < height; i++)
     {
-        // printf("      ");
-        for (int j = 1; j <= height * 2 - (2 * i - 1); j++)
+        for (int j = 1; j <= height * 2 - i; j++)
         {
             printf(" ");
-            // printf("%i |", j);
-            // printf("%i |", height * 2 - (3 * i));
         }
         make_bricks(i, height, true);
         make_bricks(i, height, false);
-        //  for (int j = 1; j <= height * 2 - (2 * i); j++)
-        // {
-        //     printf(" ");
-        // }
-        printf("\n");
 
+        printf("\n");
     }
     printf("\n");
 }
 
-void make_bricks(int quantity_of_bricks, int height, bool right_side)
+void make_bricks(int quantity_of_bricks)
 {
 
     for (int j = 0; j <= quantity_of_bricks + 1; j++)
@@ -36,7 +29,6 @@ void make_bricks(int quantity_of_bricks, int height, bool right_side)
         if (j == quantity_of_bricks + 1)
         {
             printf("  ");
-
         }
         else
         {
