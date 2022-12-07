c166677f282b74a60824a37ffacb95a6ccfe338b
Matheus <matheuskael.silva@gmail.com>
2022-12-07 14:39:40 +0000

    Wed, Dec 7, 2022, 2:39 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,19 +5,39 @@
 // MasterCard numbers start with 51, 52, 53, 54, or 55
 // Visa numbers start with 4.
 
+int findStartNumbers(long number);
+
+void checkCreditCardType(int number);
+
 int main(void)
 {
     long number = get_long("Number: ");
 
-    long start_numbers = number;
+    long start_numbers = findStartNumbers(number);
 
-    do
-    {
-        start_numbers = start_numbers / 10;
 
-    } while (start_numbers >= 100);
+    checkCreditCardType(start_numbers);
 
-    printf("%li\n", start_numbers);
 }
 
+void checkCreditCardType(int number) {
+    if( number < 10) {
+        printf("Visa.");
+    }  else if ( number > 10 & number < 50 ) {
+        printf("American Express.");
+    } else {
+        printf("MasterCard.");
+    }
+    printf("\n");
+}
 
+int findStartNumbers(long number)
+{
+    do
+    {
+        number = number / 10;
+
+    } while (number >= 100);
+
+    return number;
+}
