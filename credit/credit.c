b96bf30867f5e5eb6b06d4b0aa7b2adbba7a636f
Matheus <matheuskael.silva@gmail.com>
2022-12-07 18:13:43 +0000

    Wed, Dec 7, 2022, 6:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// American Express numbers start with 34 or 37 -> Valid number 37184482421->6<-5->6<-0
+// American Express numbers start with 34 or 37 -> Valid number 371844824216560
 // MasterCard numbers start with 51, 52, 53, 54, or 55
 // Visa numbers start with 4.
 
@@ -23,7 +23,7 @@ int main(void)
 
 
         checksum+= (second_to_last_digit % 100) / 10;
-        second_to_last_digit /= 10;
+        second_to_last_digit /= 100;
 
         printf("%i |", checksum);
         count++;
