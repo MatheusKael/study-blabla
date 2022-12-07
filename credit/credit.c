7cc27263559fbff3c71c4d339f35e565e5ec3552
Matheus <matheuskael.silva@gmail.com>
2022-12-07 20:15:30 +0000

    Wed, Dec 7, 2022, 8:15 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,16 +7,16 @@
 
 int check_if_number_is_two_digits(int number, int checksum);
 int find_start_numbers(long number);
-void check_credit_type(int number,  int number_of_digits);
+void check_credit_type(int number, int number_of_digits);
 
 int main(void)
 {
     long number = get_long("Number: ");
 
     int checksum = 0;
-    int checksum_second = 0;
     long second_to_last_digit = number;
     long first_to_last_digit = number;
+    long first_digit_result = 0;
     long start_numbers = find_start_numbers(number);
     int number_of_digits = 0;
 
@@ -26,28 +26,21 @@ int main(void)
         ++number_of_digits;
     } while (number != 0);
 
-
-int count = 0;
+    int count = 0;
     do
-
     {
         // 4003600000000014 -> 1.4 -> 4
-        long first_digit_result = (first_to_last_digit % 100) % 10;
-        // printf("%li | ", first_digit_result);
-
+        first_digit_result = (first_to_last_digit % 100) % 10;
         // 4003600000000014 -> 1.4 -> 1 * 2 = 2
-
         long second_digit_result = ((second_to_last_digit % 100) / 10) * 2;
-        checksum_second += first_digit_result;
-        checksum = check_if_number_is_two_digits(second_digit_result, checksum);
+
+        checksum = check_if_number_is_two_digits(second_digit_result, checksum) + first_digit_result;
         first_to_last_digit /= 100;
         second_to_last_digit /= 100;
 
         count++;
-    } while (count < number_of_digits);
+    } while (count < 20);
 
-
-    printf("%i", number_of_digits);
     if (checksum % 10 == 0)
     {
         check_credit_type(start_numbers, number_of_digits);
@@ -78,7 +71,7 @@ int check_if_number_is_two_digits(int number, int checksum)
 void check_credit_type(int number, int number_of_digits)
 {
 
-    if (number / 10 == 4 & number_of_digits == 13 || number_of_digits == 16)
+    if (number / 10 == 4 & number_of_digits == 16 || number_of_digits == 13)
     {
         printf("VISA");
     }
