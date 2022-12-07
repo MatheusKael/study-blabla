49c18698fe3b27ead368f79a49ef954586b7fecb
Matheus <matheuskael.silva@gmail.com>
2022-12-07 18:09:52 +0000

    Wed, Dec 7, 2022, 6:09 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -22,7 +22,7 @@ int main(void)
     {
 
 
-        checksum+= (second_to_last_digit % 100) % 10;
+        checksum+= (second_to_last_digit % 100) / 10;
         second_to_last_digit /= 10;
 
         printf("%i |", checksum);
