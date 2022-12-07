03d41516577721fc7f236e2c6979b6fb4b5e8990
Matheus <matheuskael.silva@gmail.com>
2022-12-07 18:54:38 +0000

    Wed, Dec 7, 2022, 6:54 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,9 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// American Express numbers start with 34 or 37 -> Valid number 371844824216560
-// MasterCard numbers start with 51, 52, 53, 54, or 55
-// Visa numbers start with 4. -> 4003600000000014
+// American Express StartNumbers (findStartNumbers) with 34 or 37 -> Valid number 371844824216560
+// MasterCard StartNumbers with 51, 52, 53, 54, or 55
+// Visa StartNumbers with 4. -> 4003600000000014
 
 int check_if_result_is_bigger(int number, int checksum);
 int findStartNumbers(long number);
@@ -21,19 +21,24 @@ int main(void)
     do
     {
 
-        long first_digit_result =  (second_to_last_digit % 100) % 10;
+        long first_digit_result = (second_to_last_digit % 100) % 10;
         long second_digit_result = ((second_to_last_digit % 100) / 10) * 2;
-
 
         checksum = check_if_result_is_bigger(second_digit_result, checksum) + first_digit_result;
         first_digit_result /= 100;
         second_to_last_digit /= 100;
 
-        printf("%i |", checksum);
         count++;
     } while (count < 20);
 
-    checkCreditCardType(start_numbers);
+    if (checksum % 10 == 0)
+    {
+        checkCreditCardType(start_numbers);
+    }
+    else
+    {
+        printf("INVALID");
+    }
 }
 
 int check_if_result_is_bigger(int number, int checksum)
