77e148aef1ff1b6fcf3a5ee05bc0712e3848e89d
Matheus <matheuskael.silva@gmail.com>
2022-12-07 14:06:55 +0000

    Wed, Dec 7, 2022, 2:06 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,15 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// American Express numbers start with 34 or 37
+// American Express numbers start with 34 or 37 -> Valid number 371844824216560
 // MasterCard numbers start with 51, 52, 53, 54, or 55
 // Visa numbers start with 4.
+
 
 int main(void)
 {
     int number = get_int("Number: ");
 
-    
+
 
     printf("%i\n", number);
 
