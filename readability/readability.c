47c0db4c1b005f62e3a0475f6ffe59192746f06f
Matheus <matheuskael.silva@gmail.com>
2022-12-09 14:16:49 +0000

    Fri, Dec 9, 2022, 2:16 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,6 +6,7 @@ int main(void)
 {
     int number_of_letters =0 ;
     int number_of_sentences= 0;
+    int number_of_words = 0;
     int average_number_of_letters = 0;
     int average_number_of_sentences = 0;
     int index;
@@ -17,10 +18,16 @@ int main(void)
     for(int i = 0; i < s_length; i++) {
         if(s[i] == 46 || s[i] == 33 || s[i] == 63) {
             ++number_of_sentences;
-        } else if() {}
+        } else if( s[i] != 32) {
+            ++number_of_letters;
+        }
+
     }
 
-    average_number_of_sentences = number_of_sentences
+    // Average number of sentences per 100 words.
+    average_number_of_sentences = number_of_sentences / number_of_words * 100.0;
+    average_number_of_letters = number_of_letters / number_of_words * 100.0;
+
     index = 0.0588 * average_number_of_letters - 0.296 * average_number_of_sentences - 15.8;
     printf("%i", number_of_sentences);
     printf("\n");
