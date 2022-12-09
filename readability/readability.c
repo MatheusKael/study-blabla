87004fbfaa7df0093a450ec20ff3a898f04a56fb
Matheus <matheuskael.silva@gmail.com>
2022-12-09 14:08:03 +0000

    Fri, Dec 9, 2022, 2:08 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <cs50.h>
 #include <stdio.h>
 
@@ -5,4 +6,17 @@ int main(void)
 {
     int number_of_letters =0 ;
     int number_of_sentences= 0;
+
+    string s = get_string("");
+
+    int s_length = strlen(s);
+
+    for(int i = 0; i < s_length; i++) {
+        if(s[i] == 46) {
+            ++number_of_sentences;
+        }
+    }
+
+    printf("%i", number_of_sentences);
+    printf("\n");
 }
