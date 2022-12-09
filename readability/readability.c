8c10e37aaa88232a9b9a7f6b97c81750ae51858b
Matheus <matheuskael.silva@gmail.com>
2022-12-09 14:42:03 +0000

    Fri, Dec 9, 2022, 2:42 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-    int number_of_letters =0 ;
-    int number_of_sentences= 0;
+    int number_of_letters = 0;
+    int number_of_sentences = 0;
     int number_of_words = 0;
     double average_number_of_letters = 0;
     double average_number_of_sentences = 0;
@@ -15,12 +15,18 @@ int main(void)
 
     int s_length = strlen(s);
 
-    for(int i = 0; i < s_length; i++) {
-        if(s[i] == 46 || s[i] == 33 || s[i] == 63) {
+    for (int i = 0; i < s_length; i++)
+    {
+        if (s[i] == 46 || s[i] == 33 || s[i] == 63)
+        {
             ++number_of_sentences;
-        } else if( s[i] != 32) {
+        }
+        else if (s[i] != 32)
+        {
             ++number_of_letters;
-        } else if (s[i] == 32 & i != 0) {
+        }
+        else if (s[i] == 32 & i != 0)
+        {
             ++number_of_words;
         }
     }
@@ -28,17 +34,12 @@ int main(void)
     // Congratulations! Today is your day. You're off to Great Places! You're off and away!
     // Adding +1 to numbers of words.
     ++number_of_words;
-    printf("%i\n", number_of_letters);
-    printf("%i\n", number_of_words);
-
     // Average number of sentences per 100 words.
-    average_number_of_sentences = number_of_sentences / number_of_words * 100.0;
-    printf("%i\n", number_of_sentences);
+    average_number_of_sentences = (float)number_of_sentences / (float)number_of_words * 100.0;
     // Average number of letteers per 100 words.
-    average_number_of_letters = number_of_letters / number_of_words * 100;
-    printf("%f\n", (float)number_of_letters / (float)number_of_words * 100 );
+    average_number_of_letters = (float)number_of_letters / (float)number_of_words * 100;
 
-    index = 0.0588 * average_number_of_letters - 0.296 * average_number_of_sentences - 15.8;
-    printf("%f", index);
+    index = 0.0588 * (float)average_number_of_letters - 0.296 * (float)average_number_of_sentences - 15.8;
+    printf("grade %i", (int) index);
     printf("\n");
 }
