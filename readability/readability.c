f7cb60d7791ac11cb325e609fb0dcc28ed51586f
Matheus <matheuskael.silva@gmail.com>
2022-12-09 14:10:31 +0000

    Fri, Dec 9, 2022, 2:10 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,17 +6,19 @@ int main(void)
 {
     int number_of_letters =0 ;
     int number_of_sentences= 0;
+    int index;
 
     string s = get_string("");
 
     int s_length = strlen(s);
 
     for(int i = 0; i < s_length; i++) {
-        if(s[i] == 46) {
+        if(s[i] == 46 || s[i] == 33 || s[i] == 63) {
             ++number_of_sentences;
-        }
+        } else if() {}
     }
 
+    index = 0.0588 * number_of_letters - 0.296 * number_of_sentences - 15.8;
     printf("%i", number_of_sentences);
     printf("\n");
 }
