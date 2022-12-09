926ebba88a1d7c5eec03747da1c31a53722d9837
Matheus <matheuskael.silva@gmail.com>
2022-12-09 15:15:47 +0000

    Fri, Dec 9, 2022, 3:15 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 
 int count_letters(string s);
+int count_sentences(string s);
+int count_words(string s);
 
 int main(void)
 {
-    int number_of_letters = 0;
-    int number_of_sentences = 0;
-    int number_of_words = 0;
+
     double average_number_of_letters = 0;
     double average_number_of_sentences = 0;
     double index;
@@ -19,23 +19,11 @@ int main(void)
 
     int s_length = strlen(s);
 
-    for (int i = 0; i < s_length; i++)
-    {
-        if (s[i] == 46 || s[i] == 33 || s[i] == 63)
-        {
-            ++number_of_sentences;
-        }
-        else if (isalpha(s[i]))
-        {
-            ++number_of_letters;
-        }
-        else if (s[i] == 32 & i != 0)
-        {
-            ++number_of_words;
-        }
-    }
     // Adding +1 to numbers of words.
-    ++number_of_words;
+    int number_of_letters = count_letters(s);
+    int number_of_sentences = count_sentences(s);
+    int number_of_words = count_words(s);
+
     printf("%i\n", number_of_words);
     printf("%i\n", number_of_sentences);
     printf("%i\n", number_of_letters);
@@ -48,11 +36,63 @@ int main(void)
     printf("%f\n", average_number_of_letters);
 
     index = 0.0588 * (float)average_number_of_letters - 0.296 * (float)average_number_of_sentences - 15.8;
-    printf("grade %i",  (int) round(index));
+
+    int final_result = (int) round(index);
+    if(final_result >= 16) {
+
+    printf("grade 16+");
+    }
+
+    printf("grade %i", final_result);
+
     printf("\n");
 }
 
+int count_words(string s)
+{
+    int s_length = strlen(s);
+    int number_of_words = 0;
 
-int count_letters(string s) {
+    for (int i = 0; i < s_length; i++)
+    {
 
+        if (s[i] == 32 & i != 0)
+        {
+            ++number_of_words;
+        }
+    }
+
+    ++number_of_words;
+
+    return number_of_words;
+}
+
+int count_sentences(string s)
+{
+    int s_length = strlen(s);
+    int number_of_sentences = 0;
+    for (int i = 0; i < s_length; i++)
+    {
+        if (s[i] == 46 || s[i] == 33 || s[i] == 63)
+        {
+            ++number_of_sentences;
+        }
+    }
+    return number_of_sentences;
+}
+
+int count_letters(string s)
+{
+    int s_length = strlen(s);
+    int number_of_letters = 0;
+    for (int i = 0; i < s_length; i++)
+    {
+
+        if (isalpha(s[i]))
+        {
+            ++number_of_letters;
+        }
+    }
+
+    return number_of_letters;
 }
