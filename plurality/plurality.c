9bb4375ca61362e9a01bc676b393b107c4a656dc
Matheus <matheuskael.silva@gmail.com>
2022-12-11 22:13:39 +0000

    Sun, Dec 11, 2022, 10:13 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -100,7 +100,7 @@ void print_winner(void)
 
     }
 
-    for (int i = 0; i < win; i++)
+    for (int i = 0; i < win -1; i++)
     {
             // printf("%s %i\n", winner[i].name, winner[i].votes);
         if (winner[i].votes >= winner[i + 1].votes)
