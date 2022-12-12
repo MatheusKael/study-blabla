3773967823df548d572d763416a05f5c4d2b6d6b
Matheus <matheuskael.silva@gmail.com>
2022-12-12 20:50:37 +0000

    Mon, Dec 12, 2022, 8:50 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,12 +41,13 @@ int main(int argc, char *argv[])
 
     fwrite(&header, sizeof(uint8_t), HEADER_SIZE, output);
 
-
     // TODO: Read samples from input file and write updated data to output file
 
-    int16_t buffer; 
-
-
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, ))
+    {
+        printf("%c", c);
+    }
 
     // Close files
     fclose(input);
