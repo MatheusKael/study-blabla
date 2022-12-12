c2bb8aceae834484a2df6f976a188dc3824f7d6d
Matheus <matheuskael.silva@gmail.com>
2022-12-12 02:57:07 +0000

    Mon, Dec 12, 2022, 2:57 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -114,6 +114,7 @@ int main(int argc, string argv[])
         }
 
         // Eliminate anyone with minimum number of votes
+        printf("%i", min);
         eliminate(min);
 
         // Reset vote counts back to zero
