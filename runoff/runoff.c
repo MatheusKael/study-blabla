837348f11e4fb4e4e744d246535d0272e76eea31
Matheus <matheuskael.silva@gmail.com>
2022-12-12 01:22:46 +0000

    Mon, Dec 12, 2022, 1:22 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -132,7 +132,7 @@ bool vote(int voter, int rank, string name)
 
     for(int i = 0; i < candidate_count; i++)  {
         if(strcmp(candidates[i].name, name)) {
-            preferences[voter][rank].name = name;
+            preferences[voter][rank] = name;
             return true
         }
     }
