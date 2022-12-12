12d3a94f826f14f946912ccce3ef493ba5b9a6db
Matheus <matheuskael.silva@gmail.com>
2022-12-12 20:38:04 +0000

    Mon, Dec 12, 2022, 8:38 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -37,13 +37,14 @@ int main(int argc, char *argv[])
 
     uint8_t header[HEADER_SIZE];
 
-    fread(&header, sizeof(uint8_t), HEADER_SIZE, input );
+    fread(&header, sizeof(uint8_t), HEADER_SIZE, input);
 
-    fwrite(&)
-    printf("\n");
+    fwrite(&header, sizeof(uint8_t), HEADER_SIZE, output);
 
 
     // TODO: Read samples from input file and write updated data to output file
+
+        
 
     // Close files
     fclose(input);
