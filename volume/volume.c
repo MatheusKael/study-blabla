a2dff7a409f1919874ecd1a2495f664dae6a8977
Matheus <matheuskael.silva@gmail.com>
2022-12-12 20:47:17 +0000

    Mon, Dec 12, 2022, 8:47 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -44,7 +44,9 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
 
-        
+    int16_t samples; 
+
+
 
     // Close files
     fclose(input);
