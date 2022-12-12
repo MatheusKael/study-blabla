db3c749dcd3ffc5a0137e11758a9c54afcc7abe8
Matheus <matheuskael.silva@gmail.com>
2022-12-12 13:31:41 +0000

    Mon, Dec 12, 2022, 1:31 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -152,6 +152,7 @@ void tabulate(void)
             int choice = preferences[i][j];
             if (candidates[choice].eliminated == false)
             {
+                printf("%s %i\n", candidates[choice].name, candidates[choice].votes);
                 candidates[choice].votes++;
                 break;
             }
