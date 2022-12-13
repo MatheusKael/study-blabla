bbec15ec7eb5aa163a5004a42bc1084e1df7ef1b
Matheus <matheuskael.silva@gmail.com>
2022-12-13 17:15:38 +0000

    Tue, Dec 13, 2022, 5:15 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -39,22 +39,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
 
-            float newRed = (.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue);
-            float newGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue);
-            float newBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue);
+            float *newRed = &((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue));
+            float *newGreen = &((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue));
+            float *newBlue = &((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue));
 
-            if (newRed > 255)
+            if (*newRed > 255)
             {
-                newRed = 255;
+                *newRed = 255;
             }
-            else if (newGreen > 255)
+            else if (*newGreen > 255)
             {
-                newGreen = 255;
+                *newGreen = 255;
             }
 
-            image[i][j].rgbtRed = round(newRed);
-            image[i][j].rgbtGreen = round(newGreen);
-            image[i][j].rgbtBlue = round(newBlue);
+            image[i][j].rgbtRed = round(*newRed);
+            image[i][j].rgbtGreen = round(*newGreen);
+            image[i][j].rgbtBlue = round(*newBlue);
             // printf("%i %i", image[i][j].rgbtRed, image[i][j].rgbtGreen);
         }
     }
