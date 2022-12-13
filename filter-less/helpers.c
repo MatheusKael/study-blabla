9ba06a05354dda386ddc948eeb6ea92a2c10051c
Matheus <matheuskael.silva@gmail.com>
2022-12-13 14:08:41 +0000

    Tue, Dec 13, 2022, 2:08 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -11,7 +11,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         {
             // Verify if pixel is high, and turn it high too
             long quotient, remainder;
-            char hexadecimalnum[100];
+            int hexadecimalnum;
+            int k = 0;
 
             int average = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
 
@@ -20,13 +21,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             {
                 remainder = quotient % 16;
                 if (remainder < 10)
-                    hexadecimalnum[j++] = 48 + remainder;
+                    hexadecimalnum = 48 + remainder;
                 else
-                    hexadecimalnum[j++] = 55 + remainder;
+                    hexadecimalnum = 55 + remainder;
                 quotient = quotient / 16;
             }
 
-            printf("%s", hexadecimalnum);
+            image[i][j].rgbtRed = hexadecimalnum;
+            image[i][j].rgbtGreen = hexadecimalnum;
+            image[i][j].rgbtBlue = hexadecimalnum;
+            printf("%i", hexadecimalnum);
         }
     }
 
