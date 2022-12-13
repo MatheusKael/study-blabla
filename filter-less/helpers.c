6b5829a94466001821a13322c484a245f10b2fcb
Matheus <matheuskael.silva@gmail.com>
2022-12-13 13:31:28 +0000

    Tue, Dec 13, 2022, 1:31 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -9,9 +10,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Verify if pixel is high, and turn it high too
-            image[i][j].rgbtBlue = 0x20;
-            image[i][j].rgbtGreen= 0x20;
-            image[i][j].rgbtRed= 0x20;
+            printf("%i |",image[i][j].rgbtBlue);
         }
     }
 
