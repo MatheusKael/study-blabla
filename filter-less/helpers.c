4e0a047ac9a9b357549654ed3aac2b38deb57d12
Matheus <matheuskael.silva@gmail.com>
2022-12-13 14:57:56 +0000

    Tue, Dec 13, 2022, 2:57 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -41,9 +41,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
 
-            float newRed = (.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + .189 * image[i][j].rgbtBlue;
-            float newGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + .168 * image[i][j].rgbtBlue;
-            float newBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + .131 * image[i][j].rgbtBlue;
+            float newRed = (.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue);
+            float newGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue);
+            float newBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue);
 
             if (newRed > 255)
             {
@@ -58,7 +58,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
                 newBlue = 255;
             }
 
-            printf("%f |",newBlue);
+            printf("%f |", newBlue);
             image[i][j].rgbtRed = round(newRed);
             image[i][j].rgbtGreen = round(newGreen);
             image[i][j].rgbtBlue = round(newBlue);
