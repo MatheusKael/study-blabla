bab6f4bb600fe4e54b91c2579bc7cb00b095b90f
Matheus <matheuskael.silva@gmail.com>
2022-12-13 17:19:02 +0000

    Tue, Dec 13, 2022, 5:19 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -43,15 +43,16 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float newGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue);
             float newBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue);
 
-            int *address1 = &newRed;
+            float *address1 = &newRed;
+            float *address2 = &newGreen;
 
             if (*address1 > 255)
             {
                 *address1 = 255;
             }
-            else if (newGreen > 255)
+            else if (*address2 > 255)
             {
-                newGreen = 255;
+                *address2 = 255;
             }
 
             image[i][j].rgbtRed = round(newRed);
