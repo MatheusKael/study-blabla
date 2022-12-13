f8f901f64b9ba1735d2e350e3c74c83e96fe1fa1
Matheus <matheuskael.silva@gmail.com>
2022-12-13 17:38:12 +0000

    Tue, Dec 13, 2022, 5:38 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -71,10 +71,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
-        {
-    
-        }
+        int tmp = image[i];
+        
     }
 
 
