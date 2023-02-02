448cdd2e4158126ff948c5ce4a5f450446771612
Matheus <matheuskael.silva@gmail.com>
2023-02-02 19:53:04 +0000

    Thu, Feb 2, 2023, 7:53 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -18,11 +18,11 @@ int main(void)
     {
         for (int j = 1; j < height - i; j++)
         {
-            printf("#");
+            printf(" ");
         }
-        // make_bricks(i);
-        // printf("  ");
-        // make_bricks(i);
+        make_bricks(i);
+        printf("  ");
+        make_bricks(i);
 
         printf("\n");
     }
