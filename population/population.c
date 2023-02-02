2eeedb9642478a5ce37312c3e1fffe0d43fdf325
Matheus <matheuskael.silva@gmail.com>
2023-02-02 19:01:24 +0000

    Thu, Feb 2, 2023, 7:01 PM +00:00

--- population/population.c
+++ population/population.c
@@ -9,7 +9,7 @@ int main(void)
     {
         start_size = get_int("Start size: ");
     }
-    while (n < 9)
+    while (start_size < 9);
 
     int end_size;
 
@@ -18,20 +18,22 @@ int main(void)
     {
         end_size = get_int("End size: ");
     }
-    while (end_size < start_size)
+    while (end_size < start_size);
 
     // TODO: Calculate number of years until we reach threshold
 
     int new_born= start_size;
     int dead = start_size;
     int number_of_years = 0;
+
     do {
         new_born = new_born / 3;
         dead = dead / 4;
 
-        start_size
+        start_size = start_size + new_born - dead;
         number_of_years++;
-    } while (start_size < end_size)
+    } while (start_size < end_size);
 
     // TODO: Print number of years
+    printf("Number of years %i", number_of_years);
 }
