386e331058f82dc919e678b880c25d670d580f18
Matheus <matheuskael.silva@gmail.com>
2023-02-02 18:50:56 +0000

    Thu, Feb 2, 2023, 6:50 PM +00:00

--- population/population.c
+++ population/population.c
@@ -3,14 +3,26 @@
 
 int main(void)
 {
+    int start_size;
     // TODO: Prompt for start size
-    do {
-        int n = get_int("Start size: ");
-    } while(n < 9)
+    do
+    {
+        start_size = get_int("Start size: ");
+    }
+    while (n < 9)
+
+    int end_size;
 
     // TODO: Prompt for end size
+    do
+    {
+        end_size = get_int("End size: ");
+    }
+    while (end_size < start_size)
 
     // TODO: Calculate number of years until we reach threshold
+
+    
 
     // TODO: Print number of years
 }
