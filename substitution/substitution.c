35c26f4478c14f97cea6acb09fb0923cbcb9fc17
Matheus <matheuskael.silva@gmail.com>
2023-02-05 18:12:43 +0000

    Sun, Feb 5, 2023, 6:12 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
 int main(int argc, string argv[])
@@ -11,8 +12,19 @@ int main(int argc, string argv[])
     }
     string key = argv[1];
 
-    if(strlen(key) > 26) {
-        
+    if (strlen(key) > 26 || strlen(key) < 26)
+    {
+
+        printf("Key needs 26 characters!");
+        return 1;
     }
 
+    for (int i = 0; i < 26; i++)
+    {
+        if (isalpha(key[i]) != 0)
+        {
+            printf("Key contains a not alphabetical character!");
+            return 1;
+        }
+    }
 }
