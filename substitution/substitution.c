12814ae9fc7307dec24222dfbbef14fc3fe74638
Matheus <matheuskael.silva@gmail.com>
2023-02-05 18:07:32 +0000

    Sun, Feb 5, 2023, 6:07 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,10 +3,13 @@
 
 int main(int argc, string argv[])
 {
-
     if (argc > 2 || argc == 1)
     {
         printf("Executed without any command-line arguments or with more than one command-line argument");
         return 1;
     }
+    string key = argv[1];
+
+    if(strlen())
+
 }
