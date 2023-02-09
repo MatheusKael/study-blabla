1881d9c10195d24c380c4db1215692ffe297200c
Matheus <matheuskael.silva@gmail.com>
2023-02-09 20:05:00 +0000

    Thu, Feb 9, 2023, 8:05 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -112,6 +112,9 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
+    for ( int i = 0; i < candidate_count; i++ ) {
+        preferences[i][]
+    }
     return;
 }
 
