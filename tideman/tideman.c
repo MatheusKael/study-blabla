60c41118027257943ac59cb6ea278e3c6fb20430
Matheus <matheuskael.silva@gmail.com>
2023-02-10 18:14:38 +0000

    Fri, Feb 10, 2023, 6:14 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -100,7 +101,7 @@ bool vote(int rank, string name, int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidate[i]) == 0)
+        if (strcmp(name, candidates[i]) == 0)
         {
             ranks[i] = rank;
             return true;
