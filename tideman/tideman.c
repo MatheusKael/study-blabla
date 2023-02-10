8af0255eccc4b0df663178fe7f779ad635028731
Matheus <matheuskael.silva@gmail.com>
2023-02-10 18:30:36 +0000

    Fri, Feb 10, 2023, 6:30 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -118,7 +118,7 @@ void record_preferences(int ranks[])
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (i < j && ranks[i] == i)
+            if (i < j )
             {
 
                 preferences[i][j]++;
