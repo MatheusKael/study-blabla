d23bd12a9f3271006fa851820c1c1b28227435c9
Matheus <matheuskael.silva@gmail.com>
2023-02-11 22:37:29 +0000

    Sat, Feb 11, 2023, 10:37 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -199,6 +199,17 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
+    for (int i = 0; i < pair_count; i++)
+    {
+        locked[pairs[i].winner][pairs[i].loser] = true;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for(int j = 0; j < candidate_count; i++) {
+            printf("%i", locked[i][j]);
+        }
+        printf("\n");
+    }
     return;
 }
 
