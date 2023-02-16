09f285a20794b8effb8088be13daf363a0acf384
Matheus <matheuskael.silva@gmail.com>
2023-02-16 22:01:32 +0000

    Thu, Feb 16, 2023, 10:01 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -105,7 +105,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
         for (int j = 0; j < width; j++)
         {
-            float average = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+            for (int k = i - 1; k < ; k++)
+            {
+
+                for (int y = 0; y < width; y++)
+                {
+
+                }
+            }
         }
     }
 
