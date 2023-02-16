31aa999743c022130f04472a87fc18ef869b3363
Matheus <matheuskael.silva@gmail.com>
2023-02-16 18:18:53 +0000

    Thu, Feb 16, 2023, 6:18 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
 #include "helpers.h"
+#include<stdio.h>
 
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
@@ -8,7 +8,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
 
-        for (int j = 0; j < height; j++)
+        for (int j = 0; j < width; j++)
         {
             printf("%i", image[height][width].rgbtBlue);
         }
