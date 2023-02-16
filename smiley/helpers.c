4138c8ae60a5562e784a0327eb2f68dc97d1e0ae
Matheus <matheuskael.silva@gmail.com>
2023-02-16 18:36:04 +0000

    Thu, Feb 16, 2023, 6:36 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,7 +7,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < height; j++)
         {
             printf("%i", image[height][width].rgbtBlue);
 
