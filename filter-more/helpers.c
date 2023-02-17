e7e335f9baf1926dd720c199f891c31b08e7453d
Matheus <matheuskael.silva@gmail.com>
2023-02-17 21:45:03 +0000

    Fri, Feb 17, 2023, 5:45 PM -04:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -128,22 +128,22 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int y = j - 1; y <= j + 1; y++)
                 {
-                    if (k < height && y < width)
-                    {
-                        // printf("( %i %i | %i %i )", i, j ,k, y);
-                        red_sum = red_sum + (copy[k][y].rgbtRed * Gx[k][y]);
-                        red_sumY = red_sumY + (copy[k][y].rgbtRed * Gx[y][k]);
-                        // printf("(%i)", copy[k][y].rgbtBlue);
-                        blue_sum = blue_sum + (copy[k][y].rgbtBlue * Gx[k][y]);
-                        blue_sumY = blue_sumY + (copy[k][y].rgbtBlue * Gx[y][k]);
+                    // if (k < height && y < width)
+                    // {
+                    // printf("( %i %i | %i %i )", i, j ,k, y);
+                    red_sum = red_sum + (copy[k][y].rgbtRed * Gx[k][y]);
+                    red_sumY = red_sumY + (copy[k][y].rgbtRed * Gx[y][k]);
+                    // printf("(%i)", copy[k][y].rgbtBlue);
+                    blue_sum = blue_sum + (copy[k][y].rgbtBlue * Gx[k][y]);
+                    blue_sumY = blue_sumY + (copy[k][y].rgbtBlue * Gx[y][k]);
 
-                        green_sum = green_sum + (copy[k][y].rgbtGreen * Gx[k][y]);
-                        green_sumY = green_sumY + (copy[k][y].rgbtGreen * Gx[y][k]);
-                        continue;
-                    }
-                    image[i][j].rgbtRed = 0;
-                    image[i][j].rgbtBlue = 0;
-                    image[i][j].rgbtGreen = 0;
+                    green_sum = green_sum + (copy[k][y].rgbtGreen * Gx[k][y]);
+                    green_sumY = green_sumY + (copy[k][y].rgbtGreen * Gx[y][k]);
+                    // continue;
+                    // }
+                    // image[i][j].rgbtRed = 0;
+                    // image[i][j].rgbtBlue = 0;
+                    // image[i][j].rgbtGreen = 0;
                 }
             }
 
