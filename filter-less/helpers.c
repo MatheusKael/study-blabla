006990257e6767a9981b8d7a4bdf79fd7ce77c7a
Matheus <matheuskael.silva@gmail.com>
2023-02-17 17:33:33 +0000

    Fri, Feb 17, 2023, 1:33 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -130,9 +130,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
                 // printf("\n");
             }
-            image[i][j].rgbtRed = round(red_average / count_average);
-            image[i][j].rgbtBlue = round(blue_average / count_average);
-            image[i][j].rgbtGreen = round(green_average / count_average);
+            image[i][j].rgbtRed = round(red_average / 3);
+            image[i][j].rgbtBlue = round(blue_average / 3);
+            image[i][j].rgbtGreen = round(green_average / 3);
             // printf("\n");
             // printf("\n");
             // printf("\n");
