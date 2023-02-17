fa8c1b8b7385be0c65af2e66760aef9f6c694592
Matheus <matheuskael.silva@gmail.com>
2023-02-17 19:34:29 +0000

    Fri, Feb 17, 2023, 3:34 PM -04:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -102,12 +102,10 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-
             for (int k = i - 1; k <= i + 1 && k < height; k++)
             {
                 for (int y = j - 1; y <= j + 1 && y < width; y++)
                 {
-
                     if (k < height && y < width && y >= 0 && k >= 0)
                     {
                         red_average = red_average + copy[k][y].rgbtRed;
