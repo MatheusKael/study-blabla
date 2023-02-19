8d8d53eefdf847dcfe279b61b5a3054a94d8bb2f
Matheus <matheuskael.silva@gmail.com>
2023-02-19 22:39:22 +0000

    Sun, Feb 19, 2023, 6:39 PM -04:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -101,7 +101,6 @@ RGBTRIPLE kernelx3(int i, int j, int height, int width, RGBTRIPLE image[height][
 {
 
     RGBTRIPLE kernel[3][3];
-    int sum[3][3];
     int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-2, 0, 1}};
     for (int k = i - 1, count_i = 0; k <= i + 1; k++, count_i++)
     {
@@ -138,28 +137,15 @@ RGBTRIPLE kernelx3(int i, int j, int height, int width, RGBTRIPLE image[height][
             // printf("(%i) ", Gx[row][col]);
             // printf("( %i %i | %i %i )", i, j, row, col);
             // printf(" (%i)", Gx[row][col]);
-            sum[row][col] = (kernel[row][col].rgbtRed * Gx[row][col]);
-            // red_sum += (kernel[row][col].rgbtRed * Gx[row][col]);
+            red_sum = red_sum + (kernel[row][col].rgbtRed * Gx[row][col]);
             red_sumY = red_sumY + (kernel[row][col].rgbtRed * Gx[col][row]);
             // printf("(%i)", copy[k][y].rgbtBlue);
 
-            sum[row][col] = (kernel[row][col].rgbtBlue * Gx[row][col]);
-            // blue_sum += (kernel[row][col].rgbtBlue * Gx[row][col]);
+            blue_sum = blue_sum + (kernel[row][col].rgbtBlue * Gx[row][col]);
             blue_sumY = blue_sumY + (kernel[row][col].rgbtBlue * Gx[col][row]);
 
-            sum[row][col] = (kernel[row][col].rgbtGreen * Gx[row][col]);
-            // green_sum += (kernel[row][col].rgbtGreen * Gx[row][col]);
+            green_sum = green_sum + (kernel[row][col].rgbtGreen * Gx[row][col]);
             green_sumY = green_sumY + (kernel[row][col].rgbtGreen * Gx[col][row]);
-        }
-        // printf("\n");
-    }
-    int acc = 0;
-
-    for (int row = 0; row < 3; row++)
-    {
-        for (int col = 0; col < 3; col++)
-        {
-            acc = acc +  sum[row][col];
         }
         // printf("\n");
     }
@@ -167,9 +153,9 @@ RGBTRIPLE kernelx3(int i, int j, int height, int width, RGBTRIPLE image[height][
     // int red_value = round(sqrt(pow(red_sum, 2) + pow(red_sumY, 2)));
     // int blue_value = round(sqrt(pow(blue_sum, 2) + pow(blue_sumY, 2)));
     // int green_value = round(sqrt(pow(green_sum, 2) + pow(green_sumY, 2)));
-    image[i][j].rgbtRed = acc > 0 ? acc : 0;
-    image[i][j].rgbtBlue = acc > 0 ? acc : 0;
-    image[i][j].rgbtGreen = acc > 0 ? acc : 0;
+    image[i][j].rgbtRed = red_sum > 0 ? red_sum : 0;
+    image[i][j].rgbtBlue = blue_sum > 0 ? blue_sum : 0;
+    image[i][j].rgbtGreen = green_sum > 0 ? green_sum : 0;
     // printf("  %i  ", red_value);
     return image[i][j];
 }
@@ -178,7 +164,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy[height][width];
 
-    // grayscale(height, width, image);
+    grayscale(height, width, image);
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
