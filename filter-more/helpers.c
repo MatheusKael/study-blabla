fecfd3dd03c9652eaa50482fb4092fb935bc9334
Matheus <matheuskael.silva@gmail.com>
2023-02-19 19:20:33 +0000

    Sun, Feb 19, 2023, 3:20 PM -04:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -134,8 +134,8 @@ void kernelx3(int i, int j, int height, int width, RGBTRIPLE image[height][width
     {
         for (int col = 0; col < 3; col++)
         {
-            printf(" (%i)-", kernel[row][col].rgbtBlue);
-            printf("(%i) ", Gx[row][col]);
+            // printf(" (%i)-", kernel[row][col].rgbtBlue);
+            // printf("(%i) ", Gx[row][col]);
             //             printf("( %i %i | %i %i )", i, j, k, y);
             //             printf(" = ( %i )", Gx[k][y]);
             red_sum = red_sum + (kernel[row][col].rgbtRed * Gx[row][col]);
@@ -157,6 +157,7 @@ void kernelx3(int i, int j, int height, int width, RGBTRIPLE image[height][width
     image[i][j].rgbtRed = red_value;
     image[i][j].rgbtBlue = blue_value;
     image[i][j].rgbtGreen = green_value;
+    printf("  %i  ", red_value);
 }
 
 void edges(int height, int width, RGBTRIPLE image[height][width])
