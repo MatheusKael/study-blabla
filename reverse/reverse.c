496fd60c43f101219ee703281c132b4ba204287d
Matheus <matheuskael.silva@gmail.com>
2023-02-23 18:12:47 +0000

    Thu, Feb 23, 2023, 2:12 PM -04:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -7,7 +7,6 @@
 
 int check_format(WAVHEADER header);
 int get_block_size(WAVHEADER header);
-WAVHEADER read_wav_header(FILE *file);
 
 int main(int argc, char *argv[])
 {
@@ -23,9 +22,18 @@ int main(int argc, char *argv[])
     // TODO #2
     FILE *input_file_pointer = fopen(argv[1], "r");
 
-    if (input_file == NULL)
+    if (input_file_pointer == NULL)
     {
         printf("could not open the file\n");
+        return 1;
+    }
+
+    FILE *output_file_pointer = fopen(argv[2], "w");
+
+    if(output_file_pointer == NULL)
+    {
+        printf("could not open the file\n");
+        return 1;
     }
 
     // Read header
@@ -33,7 +41,7 @@ int main(int argc, char *argv[])
     WAVHEADER header;
     fread(&header, sizeof(header), 1, input_file_pointer);
 
-    fclose(input_file);
+    fclose(input_file_pointer);
 
     // Use check_format to ensure WAV format
     // TODO #4
@@ -47,7 +55,10 @@ int main(int argc, char *argv[])
 
     // Write header to file
     // TODO #6
-    fread
+    if(fwrite(&header, sizeof(header), 1, output_file_pointer) != 1) {
+        printf("Error writing header to output file\n");
+        return 1;
+    }
 
     // Use get_block_size to calculate size of block
     // TODO #7
@@ -56,25 +67,7 @@ int main(int argc, char *argv[])
     // TODO #8
 }
 
-WAVHEADER read_wav_header(FILE *file)
-{
-    WAVHEADER header;
 
-    fread(&header.chunkID, sizeof(BYTE), 4, file);
-    fread(&header.chunkSize, sizeof(DWORD), 1, file);
-    fread(&header.format, sizeof(BYTE), 4, file);
-    fread(&header.subchunk1ID, sizeof(BYTE), 4, file);
-    fread(&header.subchunk1Size, sizeof(DWORD), 1, file);
-    fread(&header.audioFormat, sizeof(WORD), 1, file);
-    fread(&header.numChannels, sizeof(WORD), 1, file);
-    fread(&header.sampleRate, sizeof(DWORD), 1, file);
-    fread(&header.byteRate, sizeof(DWORD), 1, file);
-    fread(&header.bitsPerSample, sizeof(WORD), 1, file);
-    fread(&header.subchunk2ID, sizeof(BYTE), 4, file);
-    fread(&header.subchunk2Size, sizeof(DWORD), 1, file);
-
-    return header;
-}
 int check_format(WAVHEADER header)
 {
     // TODO #4
