c374720f561f291414ab35a8099752f401ffa901
Matheus <matheuskael.silva@gmail.com>
2023-02-23 19:10:18 +0000

    Thu, Feb 23, 2023, 3:10 PM -04:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -62,6 +62,7 @@ int main(int argc, char *argv[])
 
     // Use get_block_size to calculate size of block
     // TODO #7
+    int block_size = get_block_size(header);
 
     // Write reversed audio to file
     // TODO #8
@@ -103,5 +104,7 @@ int check_format(WAVHEADER header)
 int get_block_size(WAVHEADER header)
 {
     // TODO #7
-    return 0;
+
+
+    return header.numChannels * header.bitsPerSample;
 }
