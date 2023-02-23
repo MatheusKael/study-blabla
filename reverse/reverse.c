af57d4af93b71f160f9ae5f28fa4aae2eb7f1942
Matheus <matheuskael.silva@gmail.com>
2023-02-23 18:39:36 +0000

    Thu, Feb 23, 2023, 2:39 PM -04:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -41,7 +41,8 @@ int main(int argc, char *argv[])
     // TODO #3
     WAVHEADER header = read_wav_header(input_file_pointer);
 
-    fclose(input_file_pointer);
+
+    rewind(input_file_pointer);
 
     // Use check_format to ensure WAV format
     // TODO #4
