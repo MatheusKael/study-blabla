b12d6f034006bae55a58ca969363f0b752a952b1
Matheus <matheuskael.silva@gmail.com>
2023-02-24 21:48:38 +0000

    Fri, Feb 24, 2023, 5:48 PM -04:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -57,11 +57,11 @@ int main(int argc, char *argv[])
 
     fread(buffer, sizeof(short), audio_size / block_size, input_file_pointer);
 
-    for (int i = 0; i < audio_size / block_size / 2; i++)
+    for (int i = 0; i < num_samples/ 2; i++)
     {
         short tmp = buffer[i];
-        buffer[i] = buffer[audio_size / block_size  - i - 1];
-        buffer[audio_size / block_size - i - 1] = tmp;
+        buffer[i] = buffer[num_samples  - i - 1];
+        buffer[num_samples - i - 1] = tmp;
     }
 
     fwrite(buffer, sizeof(short), audio_size/block_size, output_file_pointer);
