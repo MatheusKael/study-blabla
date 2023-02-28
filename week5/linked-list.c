f9845dbaf499660ceb8d9659c503f86816d4dd62
Matheus <matheuskael.silva@gmail.com>
2023-02-28 17:43:49 +0000

    Tue, Feb 28, 2023, 1:43 PM -04:00

--- week5/linked-list.c
+++ week5/linked-list.c
@@ -9,9 +9,17 @@ typedef struct node {
 node;
 
 
-int main (void) {
+int main (int argc, char *argv[]) {
 
+    node *list = null;
 
+    for(int i = 1; i < argc; i++) {
+        int number = atoi(argv[i]);
+        node *n = malloc(sizeof(node));
+        if(n == NULL) {
+            return 1;
+        }
+    }
 
 
 }
