369bcd6f42c1149d9ebfd1c8bdaa5a34a525791e
Matheus <matheuskael.silva@gmail.com>
2023-03-01 18:29:25 +0000

    Wed, Mar 1, 2023, 2:29 PM -04:00

--- week5/binary-search-tree.c
+++ week5/binary-search-tree.c
@@ -13,6 +13,38 @@ typedef struct node {
 } node;
 
 
-void free_tree(node *root) {
-    
+void free_tree(node *root);
+void print_tree(node *root);
+
+
+int main(void) {
+    node *tree = NULL;
+
+    node *n = malloc(sizeof(node));
+
+    if(n == NULL) {
+        return 1;
+    }
+
+    n -> number = 2;
+    n -> left = NULL;
+    n -> right = NULL;
+    tree = n;
+    n = malloc(sizeof(node));
+
+    if(n == NULL) {
+
+        free_tree(tree);
+        return 1;
+    }
+
+    n -> number = 1;
+    n -> left = NULL;
+    n -> right = NULL;
+    tree -> left = n;
+
+
+
+
+
 }
