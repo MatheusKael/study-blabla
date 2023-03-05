82bf06a339686826409db85858c23323b9da70df
Matheus <matheuskael.silva@gmail.com>
2023-03-05 18:48:42 +0000

    Sun, Mar 5, 2023, 2:48 PM -04:00

--- week5/lab/inheritance/inheritance.c
+++ week5/lab/inheritance/inheritance.c
@@ -54,9 +54,11 @@ person *create_family(int generations)
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         int number = rand() % 2;
         int number2 = rand() % 2;
+        int number3 = rand() % 2;
+        int number4 = rand() % 2;
 
         new_person-> alleles[0] = new_person -> parents[number] -> alleles[number2];
-        new_person-> alleles[1] = new_person -> parents[number2] -> alleles[number2];
+        new_person-> alleles[1] = new_person -> parents[number3] -> alleles[number4];
 
     }
     // If there are no generations left to create
