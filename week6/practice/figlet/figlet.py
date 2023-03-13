65afc9ece1c4d4af13060e776289254cd3194d76
Matheus <matheuskael.silva@gmail.com>
2023-03-13 19:52:29 +0000

    Mon, Mar 13, 2023, 3:52 PM -04:00

--- week6/practice/figlet/figlet.py
+++ week6/practice/figlet/figlet.py
@@ -1,10 +1,21 @@
 from sys import argv, exit
 from pyfiglet import Figlet
 
-f = Figlet(font='slant')
+args = ["-f", "--font"]
 
-if (len(argv) > 2):
+argv_len = len(argv)
+
+if argv_len > 3:
     exit(1)
+elif argv_len > 1 and argv[1] in args:
 
-print(f.renderText('text to render'))
+    f = Figlet(font=argv[3])
 
+    text= input("Text to render: ")
+    print(f.renderText(text))
+else:
+
+    f = Figlet(font="slant")
+
+    text= input("Text to render: ")
+    print(f.renderText(text))
