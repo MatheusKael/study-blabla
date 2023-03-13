cc4db07b24d824be9133c02c7b79f190c7a37b92
Matheus <matheuskael.silva@gmail.com>
2023-03-13 19:58:46 +0000

    Mon, Mar 13, 2023, 3:58 PM -04:00

--- week6/practice/figlet/figlet.py
+++ week6/practice/figlet/figlet.py
@@ -5,7 +5,7 @@ args = ["-f", "--font"]
 
 argv_len = len(argv)
 
-if argv_len > 3:
+if argv_len > 3 or argv_len == 2:
     exit(1)
 elif argv_len > 1 and argv[1] in args:
 
@@ -13,9 +13,11 @@ elif argv_len > 1 and argv[1] in args:
 
     text= input("Text to render: ")
     print(f.renderText(text))
-else:
+elif argv_len == 1:
 
     f = Figlet(font="slant")
 
     text= input("Text to render: ")
     print(f.renderText(text))
+else:
+    exit(1)
