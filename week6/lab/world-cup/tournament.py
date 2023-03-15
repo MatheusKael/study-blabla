1ab1ed5346c0761355da670f215fba3c0b5af335
Matheus <matheuskael.silva@gmail.com>
2023-03-15 19:23:45 +0000

    Wed, Mar 15, 2023, 3:23 PM -04:00

--- week6/lab/world-cup/tournament.py
+++ week6/lab/world-cup/tournament.py
@@ -20,11 +20,11 @@ def main():
         reader = csv.DictReader(file)
 
         for line in reader:
-            teams.append(line['team'])
-
+            teams.append(line)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    simulate_tournament(teams)
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
