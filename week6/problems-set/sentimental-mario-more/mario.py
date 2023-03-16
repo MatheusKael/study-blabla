cde35f187ea8cd8addc23b2147a1c2bc5d64b3d9
Matheus <matheuskael.silva@gmail.com>
2023-03-16 17:21:57 +0000

    Thu, Mar 16, 2023, 1:21 PM -04:00

---
+++ week6/problems-set/sentimental-mario-more/mario.py
@@ -0,0 +1,14 @@
+def bricks(size):
+    for i in range(size):
+        for j in range(size - i):
+            print(" ", end="")
+        print("#" * i, end=" ")
+        print("#" * i)
+
+
+def main():
+    size = int(input())
+    bricks(size)
+
+
+main()
