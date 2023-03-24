9703e690695924f4e3d9211b0371df304f348c78
Matheus <matheuskael.silva@gmail.com>
2023-03-24 18:58:32 +0000

    Fri, Mar 24, 2023, 2:58 PM -04:00

--- week7/prophecy/insert.py
+++ week7/prophecy/insert.py
@@ -4,8 +4,6 @@ from cs50 import SQL
 
 db = SQL("sqlite:///roster.db")
 
-with open("./schema.sql", 'r') as schema:
-    schemas = 
 
 students = db.execute('SELECT DISTINCT house FROM students;')
 
