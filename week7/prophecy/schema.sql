f6159e09b8b082a89b2065d90f23cc0272a52e6d
Matheus <matheuskael.silva@gmail.com>
2023-03-24 17:34:00 +0000

    Fri, Mar 24, 2023, 1:34 PM -04:00

---
+++ week7/prophecy/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students (
+    id INTEGER,
+    student_name TEXT,
+    house TEXT,
+    head TEXT,
+    PRIMARY KEY(id),
+    FOR KEY
+);
+
+CREATE TABLE houses {
+    id INTEGER,
+    house_name TEXT,
+    PRIMARY KEY(id),
+}
