35fbe6c8336ba677c515ed6d971f45bd0226b3d1
Matheus <matheuskael.silva@gmail.com>
2023-03-27 17:49:57 +0000

    Mon, Mar 27, 2023, 1:49 PM -04:00

--- week7/ps/movies/11.sql
+++ week7/ps/movies/11.sql
@@ -1,6 +1,6 @@
 --In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
-SELECT title FROM movies WHERE id IN (
+SELECT title FROM movies LEFT JOIN WHERE id IN (
     SELECT movie_id FROM stars WHERE person_id = (
         SELECT id FROM people WHERE name = 'Chadwick Boseman'
     )
-) ORDER BY 
+) ORDER BY
