9acc9c14c92a3eee5f74c6d3ec2f99b367eaf1a0
Matheus <matheuskael.silva@gmail.com>
2023-03-27 17:20:08 +0000

    Mon, Mar 27, 2023, 1:20 PM -04:00

--- week7/ps/movies/6.sql
+++ week7/ps/movies/6.sql
@@ -1,2 +1,2 @@
 --In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
-SELECT avg(rating) FROM ratings WHERE
+SELECT avg(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies);
