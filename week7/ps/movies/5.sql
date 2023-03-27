8a02d1051a2baccf5c8f1f44a5c9709cdeee4106
Matheus <matheuskael.silva@gmail.com>
2023-03-27 17:16:49 +0000

    Mon, Mar 27, 2023, 1:16 PM -04:00

--- week7/ps/movies/5.sql
+++ week7/ps/movies/5.sql
@@ -1,1 +1,2 @@
 --In 5.sql, write a SQL query to list the titles and release years of all Harry Potter movies, in chronological order.
+SELECT title, year FROM movies WHERE title LIKE 'Harry potter%' ORDER BY year ASC;
