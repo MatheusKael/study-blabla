291c1321363540342318c1d6334ac79e06658004
Matheus <matheuskael.silva@gmail.com>
2023-03-27 17:42:25 +0000

    Mon, Mar 27, 2023, 1:42 PM -04:00

--- week7/ps/movies/10.sql
+++ week7/ps/movies/10.sql
@@ -1,6 +1,8 @@
 --In 10.sql, write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
 SELECT name FROM people WHERE id IN (
     SELECT person_id FROM stars WHERE movie_id IN (
-        SELECT id FROM movies WHERE 
+        SELECT id FROM movies WHERE id IN (
+            SELECT movie_id FROM ratings WHERE rating > 9.0
+        )
     )
-)
+);
