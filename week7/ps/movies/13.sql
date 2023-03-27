3c845939504d5225d07fa3b54bc8b15d0fb5b28b
Matheus <matheuskael.silva@gmail.com>
2023-03-27 18:09:17 +0000

    Mon, Mar 27, 2023, 2:09 PM -04:00

---
+++ week7/ps/movies/13.sql
@@ -0,0 +1,10 @@
+--In 13.sql, write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
+SELECT name FROM people WHERE id IN (
+    SELECT person_id FROM stars WHERE movie_id IN (
+        SELECT id FROM movies WHERE id IN (
+            SELECT movie_id FROM stars WHERE person_id = (
+                SELECT id FROM people where name = 'Kevin Bacon'
+            )
+        )
+    )
+);
