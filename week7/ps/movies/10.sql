02a267371d93ee742431755d3ea06cd99673379d
Matheus <matheuskael.silva@gmail.com>
2023-03-27 17:46:53 +0000

    Mon, Mar 27, 2023, 1:46 PM -04:00

--- week7/ps/movies/10.sql
+++ week7/ps/movies/10.sql
@@ -2,7 +2,7 @@
 SELECT distinct name FROM people WHERE id IN (
     SELECT person_id FROM directors WHERE movie_id IN (
         SELECT id FROM movies WHERE id IN (
-            SELECT movie_id FROM ratings WHERE rating > 9.0
+            SELECT movie_id FROM ratings WHERE rating >= 9.0
         )
     )
 );
