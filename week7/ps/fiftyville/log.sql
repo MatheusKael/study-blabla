6a98390628a670d33faaf34d6147b950e5bfefb1
Matheus <matheuskael.silva@gmail.com>
2023-03-28 18:17:21 +0000

    Tue, Mar 28, 2023, 2:17 PM -04:00

--- week7/ps/fiftyville/log.sql
+++ week7/ps/fiftyville/log.sql
@@ -25,3 +25,13 @@ SELECT *, phone_number AS caller FROM people WHERE caller IN (
             SELECT passport_number FROM passengers WHERE flight_id = 36)
     ) AND day = 28 AND duration < 60
 );
+
+SELECT * FROM bakery_security_logs WHERE license_plate IN (
+
+SELECT *, phone_number AS caller FROM people WHERE caller IN (
+    SELECT caller FROM phone_calls  WHERE caller IN (
+        SELECT phone_number FROM people WHERE passport_number IN (
+            SELECT passport_number FROM passengers WHERE flight_id = 36)
+    ) AND day = 28 AND duration < 60
+);
+);
