1db3c2abaceffe7c4ff52e6864797b2f9c643424
Matheus <matheuskael.silva@gmail.com>
2023-03-28 18:33:19 +0000

    Tue, Mar 28, 2023, 2:33 PM -04:00

--- week7/ps/fiftyville/log.sql
+++ week7/ps/fiftyville/log.sql
@@ -34,4 +34,6 @@ AND people.license_plate IN (
                 SELECT passport_number FROM passengers WHERE flight_id = 36)
         ) AND day = 28 AND duration < 60
     )
-) AND day = 28 AND activity = 'exit';
+) AND activity = 'entrance' AND day = 28 AND hour = 10 AND minute = 15
+    OR
+    activity = 'exit' AND day = 28 AND hour = 10;
