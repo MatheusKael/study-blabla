a2184ed9d2adf5a6a2108358203b0a0702a1726a
Matheus <matheuskael.silva@gmail.com>
2023-03-28 17:21:42 +0000

    Tue, Mar 28, 2023, 1:21 PM -04:00

--- week7/ps/fiftyville/log.sql
+++ week7/ps/fiftyville/log.sql
@@ -1,9 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 --All you know is that the theft took place on July 28, 2021 and that it took place on Humphrey Street.
 -- Select every flighst comming and going to town at ^ this date
-SELECT id FROM flights WHERE origin_airport_id =
+SELECT id, month, day, year FROM flights WHERE origin_airport_id =
     (SELECT id FROM airports WHERE city = "Fiftyville")
     OR destination_airport_id =
     (SELECT id FROM airports WHERE city = "Fiftyville")
     AND
-    year = 2021;
+    year = 2021 AND day = 28 AND month = 7;
