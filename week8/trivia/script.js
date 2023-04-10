fc36536a651d1ed9424d47ee4f431a2ebf8a8dd4
Matheus <matheuskael.silva@gmail.com>
2023-04-10 19:37:48 +0000

    Mon, Apr 10, 2023, 3:37 PM -04:00

---
+++ week8/trivia/script.js
@@ -0,0 +1,65 @@
+
+
+function part1() {
+
+    const options = document.querySelectorAll(".options > button")
+
+    const corr = document.querySelector("p.response")
+
+    for (let b of options) {
+
+        if (b.innerText == "Javascript") {
+            b.onclick = () => {
+                b.style = "background-color:green"
+
+                corr.textContent = "Correct!"
+                corr.hidden = false
+            }
+
+        } else {
+
+            b.onclick = () => {
+                b.style = "background-color:red"
+
+                corr.textContent = "Incorrect"
+                corr.hidden = false
+            }
+        }
+
+    }
+}
+
+function part2() {
+    const possibleAnswers = ["javascript", "php"]
+
+    const [input] = document.getElementsByTagName("input")
+    const send = document.querySelector(".input > button")
+    const response = document.querySelector("p.responsePart2")
+
+    send.onclick = () => {
+        const value = input.value.toLowerCase()
+
+        if (possibleAnswers.includes(value)) {
+
+            input.style = "background-color:green"
+
+            response.textContent = "Correct!"
+            response.hidden = false
+        } else {
+
+            input.style = "color:red"
+
+            response.textContent = "Incorrect"
+            response.hidden = false
+
+        }
+
+    }
+
+
+}
+
+
+
+part1()
+part2()
